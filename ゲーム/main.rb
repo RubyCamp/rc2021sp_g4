16d1d30d58509af4ca0b4e0ec2811bc53538f89e
shun72i <shun72i720@gmail.com>
2021-03-16 15:20:55 +0900

    screen_size & player_method

--- ゲーム/main.rb
+++ ゲーム/main.rb
@@ -1,5 +1,13 @@
 require 'dxruby'
 require_relative 'player.rb'
 
+Window.width = 800
+Window.height = 600
+Window.caption = "Ruby合宿_4"
+
 Window.loop do
+    player.update
+    player.draw
+
+    Sprite.check(player, Enemy.collection)
 end
