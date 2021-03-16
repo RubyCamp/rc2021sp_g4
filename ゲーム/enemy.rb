1685f721766b59f85d8d490152ea338c076c82a6
Hayato-hattori <hattori.assemble@gmail.com>
2021-03-16 13:58:01 +0900

    create hayato_branch

--- ゲーム/enemy.rb
+++ ゲーム/enemy.rb
@@ -1,1 +1,17 @@
-class enemy << Splite
+class Enemy << Splite
+    def hit(obj)
+    end
+end
+
+class Enemy_1 < Enemy
+    def update
+        self.x += 1
+        self.y += 1
+    end
+end
+
+class Enemy_2 < Enemy
+    def update
+        self.x += 1
+    end
+end
